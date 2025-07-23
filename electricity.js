10d2be036972e43d3147dd8e43229c98630b3661
Shivam Agrahari <shivamagr111102@gmail.com>
2025-07-24 01:02:58 +0530

    Electricity bill

---
+++ electricity.js
@@ -0,0 +1,18 @@
+// let unit = Number(prompt("Enter the units"))
+let unit = 700
+let amount = 0
+if(unit>400){
+  amount = (unit-400)*13;
+  unit = 400
+}
+if(unit>200 && unit<=400){
+  amount += (unit-200) * 8
+  unit = 200
+}
+if(unit>100 && unit<=200){
+  amount += (unit-100) * 6
+  unit = 100
+}
+amount += unit * 4;
+console.log(amount);
+
