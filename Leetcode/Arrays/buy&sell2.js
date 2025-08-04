f94580cc8d8b4986133ebff7e495836ff9af99ee
Shivam Agrahari <shivamagr111102@gmail.com>
2025-08-05 01:24:19 +0530

    Buy & Sell Medium level Leetcode problem

---
+++ Leetcode/Arrays/buy&sell2.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5]
+let maxProfit = 0
+for(let i=0; i<arr.length; i++){
+  let profit = 0
+  profit = arr[i+1] - arr[i]
+  if(profit>0){
+    maxProfit += profit
+  }
+}
+console.log(maxProfit);
