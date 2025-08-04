f928de35dc7fbb01531838f1992c1f99fd9a1b39
Shivam Agrahari <shivamagr111102@gmail.com>
2025-08-04 20:25:05 +0530

    Merging two sorted Array

---
+++ Leetcode/Arrays/mergesortedarray.js
@@ -0,0 +1,19 @@
+// Merge two sorted Array
+let arr1 = [2,5,6]
+let arr2 = [1,3,4,8]
+let merge = new Array(arr1.length + arr2.length)
+let i=j=k=0;
+while(i<arr1.length && j<arr2.length){
+  if(arr1[i]>arr2[j]){
+    merge[k++] = arr2[j++]
+  }else{
+    merge[k++] = arr1[i++]
+  }
+}
+while(j<arr2.length){
+  merge[k++] = arr2[j++]
+}
+while(i<arr1.length){
+  merge[k++] = arr1[i++]
+}
+console.log(merge);
